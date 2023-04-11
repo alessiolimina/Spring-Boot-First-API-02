b644c246bd1e9c65362ed92f359f135942be7fc0
Alessio Limina <alessio.limina@gmail.com>
2023-04-11 19:54:11 +0200

    Exercise Spring Boot - First API - 02

---
+++ src/main/java/it/develhope/spring/Primi/passi/First/API2/PrimiPassiFirstApi02Application.java
@@ -0,0 +1,13 @@
+package it.develhope.spring.Primi.passi.First.API2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimiPassiFirstApi02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimiPassiFirstApi02Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/develhope/spring/Primi/passi/First/API2/PrimiPassiFirstApi02ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.develhope.spring.Primi.passi.First.API2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimiPassiFirstApi02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
