76daf87f27ac517affa7f9768c30b350df75c86f
Alessio Limina <alessio.limina@gmail.com>
2023-04-12 13:20:13 +0200

    Exercise Spring Boot - First API - 02 - Updated and fixed

--- src/main/java/it/develhope/spring/Primi/passi/First/API2/controller/FactorialController.java
+++ src/main/java/it/develhope/spring/Primi/passi/First/API2/controller/FactorialController.java
@@ -1,21 +1,18 @@
 package it.develhope.spring.Primi.passi.First.API2.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class FactorialController {
 
-    @GetMapping("/")
-    public String getFactorial(@RequestParam Integer n){
+    @GetMapping("/{n}")
+    public String getFactorial(@PathVariable(required = false, value = "n") Integer n){
         long factorial = 1;
         for(int i = 1; i<=n; i++){
             factorial = factorial * i;
         }
-        return "the " + n + " factorial is " + factorial;
+        return "The " + n + " factorial is " + factorial;
 
     }
 
